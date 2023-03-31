badb0f54d084f4eb0a3b6f4be717f3fd52631537
lavet13 <lavet13@mail.ru>
2023-03-31 21:00:42 +0300

    setState & shallow merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
+// local state or just state for short is just some info that only this component is aware of and can modify
+// and could read from
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: "Yihua", lastName: "Zhang" },
+      company: "ZTM",
+    };
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
+
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
+            {this.state.company}
           </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
+
+          <button
+            onClick={() => {
+              // shallow merge technique(merging given object with the current state object)
+              this.setState({
+                name: {
+                  firstName:
+                    this.state.name.firstName === "Yihua" ? "Andrei" : "Yihua",
+                  lastName:
+                    this.state.name.lastName === "Zhang" ? "Neaogie" : "Zhang",
+                },
+              });
+              console.log(this.state);
+            }}
           >
-            Learn React
-          </a>
+            Change Name
+          </button>
         </header>
       </div>
     );
