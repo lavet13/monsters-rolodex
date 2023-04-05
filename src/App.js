b7fb1928cb37ebc7cf1bdc79c619e303227e587d
lavet13 <lavet13@mail.ru>
2023-04-05 19:53:58 +0300

    styling stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
-import CardList from './components/card-list/card-list.component';
 import './App.css';
+import SearchBox from './components/search-box/search-box.component';
+import CardList from './components/card-list/card-list.component';
 
 // local state or just state for short is just some info that only this component is aware of and can modify
 // and could read from
@@ -57,11 +58,10 @@ export default class App extends Component {
 
     return (
       <div className='App'>
-        <input
-          className='search-box'
-          type='search'
+        <SearchBox
+          className='monsters-search-box'
+          onChangeHandler={onSearchChange}
           placeholder='search monsters'
-          onChange={onSearchChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,11 +7,10 @@ export default class CardList extends Component {
 
     // components also re-render when props change(just like setState)
     // React renders on mounts and re-renders whenever props change and setState gets called
-    // and here we've seen two examples.
     const { monsters } = this.props;
 
     return (
-      <div>
+      <div className='card-list'>
         {monsters.map(monster => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
